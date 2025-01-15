a5b6f77c45faa073c783c30647238fa865cb123c
benjaminmh71 <benjaminmh71@gmail.com>
2025-01-15 18:52:33 -0500

    Random moves

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -2,4 +2,7 @@ class_name  Enemy
 extends Entity
 
 func takeAction(grid: Grid):
-	grid.move(self, 1, 0)
+	var moveOnX = randi()%2>0
+	var dx = randi()%2*2-1 if moveOnX else 0
+	var dy = 0 if moveOnX else randi()%2*2-1
+	grid.move(self, dx, dy)
--- Scripts/Entity.gd
+++ Scripts/Entity.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 var gridX
 var gridY
+var random = RandomNumberGenerator.new()
 
 func takeAction(_grid):
 	pass
